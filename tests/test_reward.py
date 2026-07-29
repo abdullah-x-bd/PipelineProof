76d51db8b5ecb187b09d6e5e3090759e448fdb16
Abdullah X <ax.zephara@gmail.com>
2026-07-29 14:36:48 +0530

    Fix release lint errors

--- tests/test_reward.py
+++ tests/test_reward.py
@@ -1,9 +1,9 @@
 import pytest
-
 from pipelineproof.soundness import reward_ladder
 
-
 pytestmark = pytest.mark.slow
+
+
 def test_reward_ladder_is_strictly_monotonic():
     report = reward_ladder()
     assert report["strictly_monotonic"], report
